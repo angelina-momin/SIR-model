7f842367472dd3e96f6c71b655e6a63171c0214f
Angelina Momin <angelina.momin53@gmail.com>
2026-09-10 20:40:08 +0800

    create config file

---
+++ config.py
@@ -0,0 +1,18 @@
+# File to store project sertings
+
+# Agent display settings
+AGENT_SHAPE = "circle"
+AGENT_RADIUS = 0.5
+AGENT_FILL = "true"
+
+DICT_COLOR_AGENTS = {
+    "susceptible": "green",
+    "infected": "red",
+    "recovered": "yellow"
+}
+
+
+# Model settings
+TOT_POP = 100 # total population
+GRID_WIDTH = 10
+GRID_HEIGHT = 10
