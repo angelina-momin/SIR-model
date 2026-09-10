54b116bcc2009543171dc99183be7076cf05afe3
Angelina Momin <angelina.momin53@gmail.com>
2026-09-10 20:06:09 +0800

    Add model grid

--- model.py
+++ model.py
@@ -3,9 +3,13 @@ from mesa import Model
 from agents import Human
 
 class DiseaseModel(Model):
-    def __init__(self, n):
+    """ Environment in which the human agents live and transmit diseases"""
+    def __init__(self, n, width, height):
         super().__init__(rng=rng)
-        self.num_agents = num_agents
+        self.no_agents = n
+
+        # Creating grid where the agents can move
+        self.grid = MultiGrid(width, height, True)
 
         Human.create_agents(model=self, n)
 
@@ -13,4 +17,3 @@ class DiseaseModel(Model):
         """ Advances the model by one step (one day) """
 
         self.agents.shuffle_do("step")
-        
