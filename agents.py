55bfe871bec8b5ed69c30e90a55257b2307d7700
Angelina Momin <angelina.momin53@gmail.com>
2026-09-10 19:49:25 +0800

    add basic agent func

--- agents.py
+++ agents.py
@@ -4,6 +4,7 @@ from mesa import Agent
 # recovered represented as booleans
 class Human(Agent):
     def __init__(self, model):
+        """ Initializes a Human instance"""
         Agent.__init__(self, model)
 
         # Initially are susceptible
@@ -11,6 +12,28 @@ class Human(Agent):
         self.infected = 0 
         self.recovered = 0
 
-    # Do they move? If so, create new function
-    
+    def move(self):
+        """ Move the agent to a random neighboring cell """
+        self.cell = self.cell.neighborhood.select_random_cell()
+
+    def susceptible_to_infected(self):
+        """ Susceptible individuals become infected """
+
+
+    def infected_to_recovered(self):
+        """ Infected individuals recover """
+
+        
+
+    def update_color(self):
+        """ Updates agents' color based on which SIR compartment
+        they belong to """
+
+    def step(self):
+        """ The behavior of the agents in a single step of the model """
+       
+        self.move()
+        self.susceptible_to_infected()
+        self.infected_to_recovered()
+        self.update_color()
         
