fcc573a8162a835866ba34fb38a6836632301709
angelina-momin <angelina.momin53@gmail.com>
2026-09-11 16:12:34 +0800

    Change infection mechanism

--- model.py
+++ model.py
@@ -12,8 +12,10 @@ class DiseaseModel(Model):
     """ Environment in which the human agents live and transmit diseases"""
     def __init__(self, n, beta, sigma, width, height, rng=None):
         super().__init__(rng=rng)
+
+        self.beta = beta
+        self.sigma = sigma
         self.no_agents = n
-        # self.schedule = RandomActivation(self) # Random selection of agents
         
         self.running =True # Necessary to run the model
         Human.create_agents(model=self, n=n)
@@ -23,18 +25,6 @@ class DiseaseModel(Model):
         with open(self.output_csv_path, mode="w", newline='') as file:
             writer = csv.writer(file)
             writer.writerow(config.OUTPUT_HEADERS)
-
-        # # Creating grid where the agents can move
-        # self.grid = OrthogonalMooreGrid((width, height))
-
-        # # Creating agents 
-        # for i in range(self.no_agents):
-        #     human = Human(i, self)
-
-        #     # Place agent in random grid cell
-        #     x = self.random.randrange(self.grid.width)
-        #     y = self.random.randrange(self.grid.height)
-        #     self.grid.place_agent(human, (x, y))
 
     def write_csv_row(self):
         """ Calculates totals in each SIR compartment and adds a data row to csv """
@@ -48,15 +38,20 @@ class DiseaseModel(Model):
             writer = csv.writer(file)
             writer.writerow([tick, tot_sus, tot_inf, tot_rec])
 
-    def draw_num_sus_to_infect(self):
+    def infect_susceptible(self):
         """ Returns number of susceptible individuals who will be infected.
         The number is a drawn sample from a binomial distribution. """
         tot_sus = sum(1 for a in self.agents if a.state == config.State.SUSCEPTIBLE)
-        no_sus_to_inf = np.random.binomial(tot_sus, self.beta)
+        no_chosen_agents = np.random.binomial(tot_sus, self.beta)
 
-        return no_sus_to_inf
+        # Pick random susceptible agents and change state
+        susceptible = [agent for agent in self.agents if agent.state == config.State.SUSCEPTIBLE]
 
-    def draw_num_inf_to_recover(self):
+        chosen = self.random.sample(susceptible, no_chosen_agents)
+        for agent in chosen:
+            agent.state = config.State.INFECTED
+
+    def recover_infected(self):
         """ Returns number of infected individuals who will recovered.
         The number is a drawn sample from a binomial distribution. """
         tot_inf = sum(1 for a in self.agents if a.state == config.State.INFECTED)
@@ -67,11 +62,11 @@ class DiseaseModel(Model):
     def step(self):
         """ Advances the model by one step (one day) """
 
+        self.infect_susceptible()
         self.agents.shuffle_do("step") # Reorders the list of agent objects
         self.write_csv_row()
 
 if __name__ == "__main__":
-    starter_model = DiseaseModel(n=1000, beta= 10, sigma=1, width=10, height=10)
-    starter_model.step()
-    starter_model.step()
-    starter_model.step()
+    starter_model = DiseaseModel(n=1000, beta= 0.1, sigma=1, width=10, height=10)
+    for _ in range(30):
+        starter_model.step()
