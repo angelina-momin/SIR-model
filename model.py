ccd70790a6761b0f175154a4c9eb61a5348d9d5a
angelina-momin <angelina.momin53@gmail.com>
2026-09-14 22:38:34 +0800

    Debug initial infections

--- model.py
+++ model.py
@@ -19,9 +19,11 @@ class DiseaseModel(Model):
         self.running =True # Necessary to run the model
         Human.create_agents(model=self, n=n)
 
-        # Creating initial number of infections
-        infc_agent = self.random.sample(population=list(self.agents), k=no_initial_infc)[0]
-        infc_agent.state = config.State.INFECTED
+        # Creating initial number of infections in random agents
+        chosen_sus_agent_list = self.random.sample(population=list(self.agents), k=no_initial_infc)
+
+        for sus_agent in chosen_sus_agent_list:
+            sus_agent.state = config.State.INFECTED
 
         # Initializing the output file
         self.output_csv_path = f'{config.OUTPUT_DIR}{output_file_name}.csv'
