7abe631824953e328c88a798b013ade8ff3ca43f
angelina-momin <angelina.momin53@gmail.com>
2026-09-14 23:51:21 +0800

    Finished test nonneg pop

--- tests/test_nonneg_pop.py
+++ tests/test_nonneg_pop.py
@@ -6,28 +6,55 @@ import unittest
 
 class TestNonNegativePopulation(unittest.TestCase):
     """ Test to ensure that none of the SIR populations are negative.
-    This is especially important as the model gets more complex over time
+    Test is done over multiple time steps of the model as defined by n_model_steps.
+    This test is especially important as the model gets more complex over time
     with birth, death rate and change in mechanics of movement of population across the compartments. """
 
-    test_model = DiseaseModel(
-        n=dict["tot_pop"], 
-        beta= dict["beta"], 
-        sigma= dict["sigma"], 
-        no_initial_infc=dict["no_initial_infc"], 
-        output_file_name=dict["output_file_name"]
-    )
+    # Initializing objects and variables that will be used in each test
+    def setUp(self):
+        self.test_model = DiseaseModel(
+            n=dict["tot_pop"], 
+            beta= dict["beta"], 
+            sigma= dict["sigma"], 
+            no_initial_infc=dict["no_initial_infc"], 
+            output_file_name=dict["output_file_name"]
+        )
 
-    n_model_steps = 10
+        self.n_model_steps = 10
 
-    def test_susceptible_population(self, test_model, n_model_steps):
+    def test_susceptible_population(self):
 
-        # Check for number of steps that the susceptible population is always >= 0
-        for step in range(n_model_steps + 1):
-            test_model.step()
-            tot_sus = sum(1 for a in self.agents if a.state == config.State.SUSCEPTIBLE)
+        # Check for each time step (including day 0), the susceptible population is not negative
+        for i in range(0, self.n_model_steps + 1):
+
+            # Not progressing the model for step 0
+            if i > 0:
+                self.test_model.step()
+
+            tot_sus = sum(1 for a in self.test_model.agents if a.state == config.State.SUSCEPTIBLE)
             self.assertGreaterEqual(tot_sus, 0)
 
-    # def test_infected_population(self, n_model_steps):
+    def test_infected_population(self):
 
-    # def test_recovered_population(self):
+        # Check for each time step (including day 0), the infected population is not negative
+        for i in range(0, self.n_model_steps + 1):
 
+            # Not progressing the model for step 0
+            if i > 0:
+                self.test_model.step()
+
+            tot_infc = sum(1 for a in self.test_model.agents if a.state == config.State.INFECTED)
+            self.assertGreaterEqual(tot_infc, 0)
+
+
+    def test_recovered_population(self):
+
+        # Check for each time step (including day 0), the recovered population is not negative
+        for i in range(0, self.n_model_steps + 1):
+
+            # Not progressing the model for step 0
+            if i > 0:
+                self.test_model.step()
+
+            tot_rec = sum(1 for a in self.test_model.agents if a.state == config.State.RECOVERED)
+            self.assertGreaterEqual(tot_rec, 0)
